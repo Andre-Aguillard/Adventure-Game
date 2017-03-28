910294a2b7ffef152e302e39e1d931c3a2a72a2e
DJSanti <samantha.e.santiago@gmail.com>
2017-03-27 20:37:25 -0500

    Update rooms 2.0.py

--- rooms 2.0.py
+++ rooms 2.0.py
@@ -153,11 +153,22 @@ class Game(Frame):
         # this needs to be global since it's changed in the main part of the program
         global currentRoom
         # add gifs later - Santiago
+        # adding all the rooms - Santiago
         r1 = Room("Room 1", "room1.gif")
         r2 = Room("Room 2", "room2.gif")
         r3 = Room("Room 3", "room3.gif")
         r4 = Room("Room 4", "room4.gif")
-        r8 = Room("Attic 1","attic1.gif") ### This room needs to be more defined, I just added it to fix somethign below - Aguillard
+        r5 = Room("the basement", "basement1.gif")
+        r6 = Room("the basement", "poster.gif")
+        r7 = Room("the basement", "basement1.gif")
+        r8 = Room("the attic","attic1.gif") ### This room needs to be more defined, I just added it to fix somethign below - Aguillard
+        r9 = Room("the attic", "attic1.gif")
+        r10 = Room("the attic", "attic1.gif")
+        r11 = Room(" ", "badend.gif")
+        r12 = Room("a tunnel", "tunnel.gif")
+        r13 = Room("a tunnel", "tunnel.gif")
+        r14 = Room("a tunnel", "tunnel.gif")
+        r15 = Room(" ", "goodend.gif")
         # adds exits to room 1
         r1.addExit("east", r2) # to the east of room 1 is room 2
         r1.addExit("south", r3)
@@ -168,12 +179,12 @@ class Game(Frame):
         # add items
         r1.addItem("window", "The window is shattered, and there are shards of glass on the floor. This is how you got into the house.")
         r1.addItem("chair", "It's a chair. It's timeworn and ripped on one side, but looks comfy nonetheless.")
-        r1.addItem("table", "It appears to be made of mahogany. A brass key \nlays on it, close to the left edge as though \ntossed there carelessly.") 
+        r1.addItem("table", "It appears to be made of mahogany. A brass key\nlays on it, close to the left edge as though\ntossed there carelessly.") 
         #add exits to room 2    ###I'm gonna format the lines like the ones above^^^ so they don't cut words in half in the window - Aguillard
         r2.addExit("west", r1)
         r2.addExit("south", r4)
         r2.addKickable("rug") #added the kickable - Santiago
-        r2.addItem("rug", "It looks like one of those Persian rugs your \n grandmother has. One of the edges has rolled up.\n You think you see something underneath.")
+        r2.addItem("rug", "It looks like one of those Persian rugs your\ngrandmother has. One of the edges has rolled up.\nYou think you see something underneath.")
         # there will be a trapdoor under the rug. Add input so that rug can be removed
         r2.addItem("fireplace", "It's a stone fireplace, with nothing but ashes in it. There is currently no fire lit.")
         #add exits to room 3
@@ -183,7 +194,7 @@ class Game(Frame):
         r3.addGrabbable("journal")
         r3.addGrabbable("flashlight")
         # add items
-        r3.addItem("bookshelves", "One shelf has its books organized by series. Another shelf is filled with knick-knacks. The others are empty.")
+        r3.addItem("bookshelves", "One shelf has its books organized by series. Another shelf is filled\nwith knick-knacks. The others are empty.")
         # may add knick-knacks to be picked up
         r3.addItem("statue", "You're unsure whether it's supposed to be a Greek bust, or if something knocked its head off.")
         r3.addItem("desk", "A faded red journal rests upon the mahogany surface.") # there should be a read option, so that you can gaze upon cryptic recipes for beer.
@@ -196,11 +207,44 @@ class Game(Frame):
         # add grabbables
         r4.addGrabbable("6-pack")
         # add items
-        r4.addItem("brew_rig", "You have no idea how to brew anything, but now \nyou know whose house you've broken into. A 6-pack of some experimental batch is resting beside it. This is what you came for.")
-        r4.addItem("painting", "The painting is of a gray-haired fellow, his head surrounded by a golden halo. The background is of a cloudy sky. You know very well that this is a \ndepiction of Our Gourd and Savior.")
+        r4.addItem("brew_rig", "You have no idea how to brew anything, but now\nyou know whose house you've broken into. A 6-pack of some\nexperimental batch is resting beside it. This is what you came for.")
+        r4.addItem("painting", "The painting is of a gray-haired fellow, his head surrounded\nby a golden halo. The background is of a cloudy sky. You know very wellthat this is a\ndepiction of Our Gourd and Savior.")
         r4.addItem("ladder", "It's a metal ladder, bolted to the left wall.")
         r4.addItem("window", "It's an open window on the far side of the room. You should really watch your step.")
+        # now adding the other rooms - Santiago
+        r5.addExit("up", r2)
+        r5.addExit("east", r6)
+        r5.addItem("light", "It's a bare bulb on a string.\nIt doesn't provide a lot to see by.")
+        r5.addItem("cobweb", "It's a dusty web.\nYou're hoping that there isn't a massive spider in it.")\
+        
+        r6.addExit("west", r5)
+        r6.addExit("east", r7)
+        r6.addItem("old_poster", "It's a faded poster of a beautiful woman; it seems familiar for some reason.\nIt's stuck to the wall with glue. You think you can hear the wind\nblowing behind it. You could use something to tear it.")
+        
+        r7.addExit("west", r6)
+        r7.addItem("box", "It's a small, wooden box atop a pedestal.\nYou're pretty sure it used to hold cigars.\nIt is locked.")
+        r7.addItem("overhead_lamp", "A large lamp hangs over the pedestal.")
+        
+        r8.addExit("down", r4)
+        r8.addExit("west", r9)
+        r8.addExit("south", r10)
+        
+        r9.addExit("east", r8)
+        r9.addItem("rock", "It's a small, round stone laying on the wooden floor. If you flip it over,\nyou can see that someone painted a face on it. Either way, it's excellent for throwing\nat things, especially things that tear.")
+        
+        r10.addExit("north", r8)
+        r10.addExit("south", None)
+        r10.addItem("window", "It is an open window. If you go up to it and look down, you can see\n a ladder leaning up against the side of the house. It's a way out!")
+        r10.addItem("hockey_stick", "It's a hockey stick, long and curved at the end. Considering you're\nin the South, and it's about 80 degrees Fahrenheit outside,\nyou wonder why this is even here.")
+        
+        r12.addExit("south", r6)
+        r12.addExit("north", r13)
+    
+        r13.addExit("south", r12)
+        r13.addExit("north", r14)
 
+        r14.addExit("south", r13)
+        
         Game.currentRoom = r1 # changed this and inventory - Santiago
         Game.inventory = [] # inventory is now here - Santiago
         
@@ -279,8 +323,7 @@ class Game(Frame):
         # to compare verb and noun to known values
         action = action.lower()
         # default response
-        response = "I don't understand. Try the format <verb> <noun>. Valid verbs are go, look, take, and kick.\n
-            Type help me for more assistance." ### I added a help response -Aguillard
+        response = "I don't understand. Try the format <verb> <noun>. Valid verbs are go, look, take, and kick.\nType help me for more assistance." ### I added a help response -Aguillard
                       
         # exit the game if player wants to leave
         # supports quit, exit, and bye, felicia
@@ -344,7 +387,7 @@ class Game(Frame):
                 for kickable in Game.currentRoom.kickables:
                     if (noun == kickable):
                         # response here
-                        response = "You've kicked the rug away, or rather, \nyou've shuffled it out of the way.\nYou've discovered a trapdoor!"
+                        response = "You've kicked the rug away, or rather,\nyou've shuffled it out of the way.\nYou've discovered a trapdoor!"
                         Game.currentRoom.addItem("trapdoor", "It is a wooden door with a circular handle.\nThe handle folds into the floor.")
             #### Help me function provides more assitance to the user -Aguillard
             ### Also \n formats the text in tkinter to a new line, I went through and made sure the text all fit 
